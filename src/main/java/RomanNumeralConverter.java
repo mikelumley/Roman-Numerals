04a2507d0d87ace35e62b9bb6900fb2305f61a13
Michael Lumley <michael.lumley@myob.com>
2019-08-26 16:00:28 +1000

    Refactored code

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,78 +1,98 @@
 public class RomanNumeralConverter {
 
-    public static String convert(int input) {
+    private static final int M = 1000;
+    private static final int D = 500;
+    private static final int C = 100;
+    private static final int L = 50;
+    private static final int X = 10;
+    private static final int V = 5;
+    private static final int I = 1;
+
+    public static String convertIntegerToRomanNumeral(int input) {
         StringBuilder output = new StringBuilder();
 
-        int numberOfThouands = input / 1000;
-        input = input % 1000;
-        output.append(RomanNumeralConverter.printNumerals('M', numberOfThouands));
+        int remainder = printThousands(input, output);
+        remainder = printFiveHundreds(output, remainder);
+        remainder = printHundreds(output, remainder);
+        remainder = printFifties(output, remainder);
+        remainder = printTens(output, remainder);
+        remainder = printFives(output, remainder);
 
-        if(input >= 900) {
-            output.append(RomanNumeralConverter.printNumerals('C', 1));
-            output.append(RomanNumeralConverter.printNumerals('M', 1));
-            input -= 900;
-        }
-
-        int numberOfFiveHundreds = input / 500;
-        input = input % 500;
-        output.append(RomanNumeralConverter.printNumerals('D', numberOfFiveHundreds));
-
-        if(input >= 400) {
-            output.append(RomanNumeralConverter.printNumerals('C', 1));
-            output.append(RomanNumeralConverter.printNumerals('D', 1));
-            input -= 400;
-        }
-
-        int numberOfHundreds = input / 100;
-        input = input % 100;
-        output.append(RomanNumeralConverter.printNumerals('C', numberOfHundreds));
-
-        if(input >= 90) {
-            output.append(RomanNumeralConverter.printNumerals('X', 1));
-            output.append(RomanNumeralConverter.printNumerals('C', 1));
-            input -= 90;
-        }
-
-        int numberOfFifties = input / 50;
-        input = input % 50;
-        output.append(RomanNumeralConverter.printNumerals('L', numberOfFifties));
-
-        if(input >= 40) {
-            output.append(RomanNumeralConverter.printNumerals('X', 1));
-            output.append(RomanNumeralConverter.printNumerals('L', 1));
-            input -= 40;
-        }
-
-        int numberOfTens = input / 10;
-        input = input % 10;
-        output.append(RomanNumeralConverter.printNumerals('X', numberOfTens));
-
-        if(input >= 9) {
-            output.append(RomanNumeralConverter.printNumerals('I', 1));
-            output.append(RomanNumeralConverter.printNumerals('X', 1));
-            input -= 9;
-        }
-
-        int numberOfFives = input / 5;
-        input = input % 5;
-        output.append(RomanNumeralConverter.printNumerals('V', numberOfFives));
-
-        if(input >= 4) {
-            output.append(RomanNumeralConverter.printNumerals('I', 1));
-            output.append(RomanNumeralConverter.printNumerals('V', 1));
-            input -= 4;
-        }
-
-        int numberOfOnes = input;
-        output.append(RomanNumeralConverter.printNumerals('I', numberOfOnes));
+        output.append(RomanNumeralConverter.printNumerals("I", remainder));
 
         return output.toString();
     }
 
-    private static String printNumerals(char numeral, int numberOfTimes) {
+    private static int printFives(StringBuilder output, int remainder) {
+        int numberOfFives = remainder / V;
+        remainder = remainder % V;
+        output.append(RomanNumeralConverter.printNumerals("V", numberOfFives));
+        if(remainder >= V - I) {
+            output.append(RomanNumeralConverter.printNumerals("IV", 1));
+            remainder -= V - I;
+        }
+        return remainder;
+    }
+
+    private static int printTens(StringBuilder output, int remainder) {
+        int numberOfTens = remainder / X;
+        remainder = remainder % X;
+        output.append(RomanNumeralConverter.printNumerals("X", numberOfTens));
+        if(remainder >= X - I) {
+            output.append(RomanNumeralConverter.printNumerals("IX", 1));
+            remainder -= X - I;
+        }
+        return remainder;
+    }
+
+    private static int printFifties(StringBuilder output, int remainder) {
+        int numberOfFifties = remainder / L;
+        remainder = remainder % L;
+        output.append(RomanNumeralConverter.printNumerals("L", numberOfFifties));
+        if(remainder >= L - X) {
+            output.append(RomanNumeralConverter.printNumerals("XL", 1));
+            remainder -= L - X;
+        }
+        return remainder;
+    }
+
+    private static int printHundreds(StringBuilder output, int remainder) {
+        int numberOfHundreds = remainder / C;
+        remainder = remainder % C;
+        output.append(RomanNumeralConverter.printNumerals("C", numberOfHundreds));
+        if(remainder >= C - X) {
+            output.append(RomanNumeralConverter.printNumerals("XC", 1));
+            remainder -= C - X;
+        }
+        return remainder;
+    }
+
+    private static int printFiveHundreds(StringBuilder output, int remainder) {
+        int numberOfFiveHundreds = remainder / D;
+        remainder = remainder % D;
+        output.append(RomanNumeralConverter.printNumerals("D", numberOfFiveHundreds));
+        if(remainder >= D - C) {
+            output.append(RomanNumeralConverter.printNumerals("CD", 1));
+            remainder -= D - C;
+        }
+        return remainder;
+    }
+
+    private static int printThousands(int input, StringBuilder output) {
+        int numberOfThousands = input / M;
+        int remainder = input % M;
+        output.append(RomanNumeralConverter.printNumerals("M", numberOfThousands));
+        if(remainder >= M - C) {
+            output.append(RomanNumeralConverter.printNumerals("CM", 1));
+            remainder -= M - C;
+        }
+        return remainder;
+    }
+
+    private static String printNumerals(String numerals, int numberOfTimes) {
         StringBuilder output = new StringBuilder();
         for(int i = 0; i < numberOfTimes; i++) {
-            output.append(numeral);
+            output.append(numerals);
         }
         return output.toString();
     }
--- src/test/java/RomanNumeralConverterTests.java
+++ src/test/java/RomanNumeralConverterTests.java
@@ -7,147 +7,154 @@ public class RomanNumeralConverterTests {
     @Test
     public void Given_Input1_When_ConvertingToRomanNumerals_Then_ReturnI() {
         int input = 1;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("I", output);
     }
 
     @Test
     public void Given_Input2_When_ConvertingToRomanNumerals_Then_ReturnII() {
         int input = 2;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("II", output);
     }
 
     @Test
     public void Given_Input3_When_ConvertingToRomanNumerals_Then_ReturnIII() {
         int input = 3;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("III", output);
     }
 
     @Test
     public void Given_Input5_When_ConvertingToRomanNumerals_Then_ReturnV() {
         int input = 5;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("V", output);
     }
 
     @Test
     public void Given_Input10_When_ConvertingToRomanNumerals_Then_ReturnX() {
         int input = 10;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("X", output);
     }
 
     @Test
     public void Given_Input6_When_ConvertingToRomanNumerals_Then_ReturnVI() {
         int input = 6;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("VI", output);
     }
 
     @Test
     public void Given_Input4_When_ConvertingToRomanNumerals_Then_ReturnIV() {
         int input = 4;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("IV", output);
     }
 
     @Test
     public void Given_Input9_When_ConvertingToRomanNumerals_Then_ReturnIX() {
         int input = 9;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("IX", output);
     }
 
     @Test
     public void Given_Input14_When_ConvertingToRomanNumerals_Then_ReturnXIV() {
         int input = 14;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("XIV", output);
     }
 
     @Test
     public void Given_Input18_When_ConvertingToRomanNumerals_Then_ReturnXVIII() {
         int input = 18;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("XVIII", output);
     }
 
     @Test
     public void Given_Input19_When_ConvertingToRomanNumerals_Then_ReturnXIX() {
         int input = 19;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("XIX", output);
     }
 
     @Test
     public void Given_Input24_When_ConvertingToRomanNumerals_Then_ReturnXXIV() {
         int input = 24;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("XXIV", output);
     }
 
     @Test
     public void Given_Input29_When_ConvertingToRomanNumerals_Then_ReturnXXIX() {
         int input = 29;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("XXIX", output);
     }
 
     @Test
     public void Given_Input40_When_ConvertingToRomanNumerals_Then_ReturnXL() {
         int input = 40;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("XL", output);
     }
 
     @Test
     public void Given_Input50_When_ConvertingToRomanNumerals_Then_ReturnL() {
         int input = 50;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("L", output);
     }
 
     @Test
     public void Given_Input54_When_ConvertingToRomanNumerals_Then_ReturnLIV() {
         int input = 54;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("LIV", output);
     }
 
     @Test
     public void Given_Input90_When_ConvertingToRomanNumerals_Then_ReturnXC() {
         int input = 90;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("XC", output);
     }
 
     @Test
     public void Given_Input400_When_ConvertingToRomanNumerals_Then_ReturnCD() {
         int input = 400;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("CD", output);
     }
 
     @Test
     public void Given_Input490_When_ConvertingToRomanNumerals_Then_ReturnCDXC() {
         int input = 490;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("CDXC", output);
     }
 
     @Test
     public void Given_Input900_When_ConvertingToRomanNumerals_Then_ReturnCM() {
         int input = 900;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("CM", output);
     }
 
     @Test
     public void Given_Input999_When_ConvertingToRomanNumerals_Then_ReturnCMXCIX() {
         int input = 999;
-        String output = RomanNumeralConverter.convert(input);
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
         assertEquals("CMXCIX", output);
+    }
+
+    @Test
+    public void Given_Input3999_When_ConvertingToRomanNumerals_Then_ReturnMMMCMXCIX() {
+        int input = 3999;
+        String output = RomanNumeralConverter.convertIntegerToRomanNumeral(input);
+        assertEquals("MMMCMXCIX", output);
     }
 }
